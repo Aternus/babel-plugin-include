d7e371c9c5e002851afff4de72f380387c0dc5b5
Aternus <kiril@atern.us>
2023-06-23 13:40:53 +0300

    FEAT: TypeScript - rewritten the plugin in TypeScript - removed babel config - tidy npm dependencies
    Signed-off-by: Aternus <kiril@atern.us>

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,44 @@
 import fs from 'fs';
-import path from 'path';
-import parser from '@babel/parser';
-import * as BabelCoreNS from '@babel/core';
-import {CustomPluginPass} from './index.types';
+import nodePath from 'path';
 
-type BabelCore = typeof BabelCoreNS;
+import {BabelCore, CustomNodePath, CustomPlugin} from './index.types';
+import {FileNotFoundError} from './index.errors';
 
-export default function (
-  babel: BabelCore,
-): BabelCoreNS.PluginObj<CustomPluginPass> {
-  console.log({babel});
-
-  const {types: t} = babel;
+function BabelPluginInclude(babel: BabelCore): CustomPlugin {
+  const {types} = babel;
   return {
     name: 'babel-plugin-include',
     visitor: {
-      CallExpression(p, state) {
-        // const name = p.node.callee.name;
-        // const args = p.node.arguments;
-        //
-        // if (name === 'include') {
-        //   // the first argument must be a string
-        //   t.assertStringLiteral(args[0]);
-        //
-        //   // get the path of the invoking file
-        //   const pathOfTheInvokingFile = this.file.opts.filename; // v7 and v6
-        //
-        //   // error checking
-        //   if (pathOfTheInvokingFile === undefined) {
-        //     throw new Error('The include function could not determine the path of the invoking file.');
-        //   }
-        //
-        //   // calculate the root directory
-        //   const root = state.opts.root || path.dirname(pathOfTheInvokingFile);
-        //
-        //   // set file encoding
-        //   const encoding = state.opts.encoding || 'utf8';
-        //
-        //   // generate the full path to the file to be included
-        //   const fileRelPath = args[0].value;
-        //   const filePath = path.join(root, fileRelPath);
-        //
-        //   // read the file
-        //   let fileSrc = fs.readFileSync(filePath, { encoding });
-        //
-        //   // if the file is a buffer, convert it to a string
-        //   if (fileSrc instanceof Buffer) {
-        //     fileSrc = fileSrc.toString(encoding);
-        //   }
-        //
-        //   // parse the file to an AST
-        //   let ast = parser.parse(fileSrc);
-        //
-        //   // take out the program body
-        //   ast = ast.program.body;
-        //
-        //   // replace the include node with the ast of the included file
-        //   p.replaceWithMultiple(ast);
-        // }
+      CallExpression(path: CustomNodePath, state) {
+        if (path.node.callee.name === 'include') {
+          const root = state.opts.root || nodePath.dirname(state.filename);
+          const encoding = state.opts.encoding || 'utf8';
+
+          const [filePathArg] = path.node.arguments;
+
+          if (filePathArg && types.isStringLiteral(filePathArg)) {
+            const includedFileName = filePathArg.value;
+            const pathToIncludedFile = nodePath.resolve(root, includedFileName);
+
+            let fileContent;
+            try {
+              fileContent = fs.readFileSync(pathToIncludedFile, encoding);
+            } catch (e) {
+              throw new FileNotFoundError(pathToIncludedFile);
+            }
+
+            const {ast} = babel.transformSync(fileContent, {
+              ast: true,
+              filename: pathToIncludedFile,
+              configFile: false, // disable the use of babel config
+              plugins: [[BabelPluginInclude, state.opts]], // enable recursive use of include()
+            });
+            path.replaceWithMultiple(ast.program.body);
+          }
+        }
       },
     },
   };
 }
+
+export default BabelPluginInclude;
--- src/index.types.ts
+++ src/index.types.ts
@@ -1,12 +1,24 @@
 import * as BabelCoreNS from '@babel/core';
 
+export type BabelCore = typeof BabelCoreNS;
+
 declare global {
-  function include(path: string): string;
+  function include(path: string): void;
 }
+
+interface IncludedCallExpression extends BabelCoreNS.types.CallExpression {
+  callee: BabelCoreNS.types.Identifier & {
+    name: string;
+  };
+}
+
+export type CustomNodePath = BabelCoreNS.NodePath<IncludedCallExpression>;
 
 export interface CustomPluginPass extends BabelCoreNS.PluginPass {
   opts: {
     root?: string;
-    encoding?: string;
+    encoding?: BufferEncoding;
   };
 }
+
+export type CustomPlugin = BabelCoreNS.PluginObj<CustomPluginPass>;
