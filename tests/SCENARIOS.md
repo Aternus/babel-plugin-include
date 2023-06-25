d5e221482d3508d78aff9c1f5c3b79ee622b0433
Aternus <kiril@atern.us>
2023-06-25 15:58:13 +0300

    FEAT: unit tests - added basic file inclusion scenario
    Signed-off-by: Aternus <kiril@atern.us>

---
+++ tests/SCENARIOS.md
@@ -0,0 +1,47 @@
+# SCENARIOS
+
+1. **Basic File Inclusion**: Test that the plugin correctly replaces
+   "include(pathToFile)" with the content of "pathToFile" when given a valid
+   path to a file.
+
+2. **File Not Found**: Test that the plugin appropriately handles the case where
+   "pathToFile" points to a file that does not exist. Depending on the expected
+   behavior, this could either throw an error or return some default value.
+
+3. **Nested Inclusions**: Test that the plugin correctly handles the case where
+   an included file itself includes another file. This will test the plugin's
+   recursive capabilities.
+
+4. **Cyclic Inclusions**: Test how the plugin handles the case where there's a
+   cycle in the inclusion (File A includes File B, and File B includes File A).
+   Depending on the intended behavior, it could either handle it gracefully,
+   throw an error, or end up in an infinite loop.
+
+5. **Relative Paths**: Test that the plugin correctly handles relative paths,
+   both in the form of "../file" and "./file".
+
+6. **Absolute Paths**: Test that the plugin correctly handles absolute paths.
+
+7. **Multiple Includes**: Test how the plugin handles a file with multiple
+   "include(pathToFile)" calls.
+
+8. **Include in Different Scopes**: Test the behavior of the plugin when the
+   include function is called inside different scopes (global scope, function
+   scope, block scope, etc.).
+
+9. **Non-String Arguments**: Test how the plugin reacts to non-string arguments
+   to the "include" function.
+
+10. **Empty Arguments**: Test how the plugin reacts to an empty argument to the
+    "include" function.
+
+11. **Whitespace Handling**: Test how the plugin handles files that have
+    leading/trailing white space or white space between elements.
+
+12. **Binary Files**: Test how the plugin handles binary files.
+
+13. **Unsupported File Types**: Test how the plugin handles unsupported file
+    types.
+
+14. **Large Files**: Test how the plugin handles very large files. This could
+    potentially reveal performance or memory usage issues.
--- tests/basic_file_inclusion.test.ts
+++ tests/basic_file_inclusion.test.ts
@@ -3,17 +3,12 @@ import {transform} from '@babel/core';
 
 import babelPluginInclude from '../src/index';
 
-const relativePathToDummyFile = 'fixtures/basic_javascript.js';
-const absolutePathToDummyFile = path.resolve(
-  __dirname,
-  relativePathToDummyFile,
-);
 const dummyFileContent = 'const x = 10;';
 
 jest.mock('fs', () => ({
   ...jest.requireActual('fs'),
   readFileSync: jest.fn().mockImplementation((path, encoding) => {
-    if (path === absolutePathToDummyFile) {
+    if (path.endsWith('basic_javascript.js')) {
       return dummyFileContent;
     }
 
@@ -22,8 +17,8 @@ jest.mock('fs', () => ({
 }));
 
 describe('babel-plugin-include', () => {
-  it('replaces include(pathToFile) with the content of pathToFile', () => {
-    const code = `include("${relativePathToDummyFile}");`;
+  it('replaces include("relativePathToFile") with the content of pathToFile', () => {
+    const code = 'include("./fixtures/basic_javascript.js");';
     const expectedCode = dummyFileContent;
 
     const result = transform(code, {
@@ -34,8 +29,49 @@ describe('babel-plugin-include', () => {
     expect(result?.code?.trim()).toEqual(expectedCode);
   });
 
+  it('replaces include("absolutePathToFile") with the content of pathToFile', () => {
+    const absolutePathToFile = path.resolve(
+      __dirname,
+      './fixtures/basic_javascript.js',
+    );
+    const code = `include("${absolutePathToFile}");`;
+    const expectedCode = dummyFileContent;
+
+    const result = transform(code, {
+      filename: __filename,
+      plugins: [[babelPluginInclude]],
+    });
+
+    expect(result?.code?.trim()).toEqual(expectedCode);
+  });
+
+  it('uses root option and relative path to include file', () => {
+    const rootDir = path.resolve(__dirname, './fixtures');
+    const code = 'include("basic_javascript.js");';
+    const expectedCode = dummyFileContent;
+
+    const result = transform(code, {
+      filename: __filename,
+      plugins: [[babelPluginInclude, {root: rootDir}]],
+    });
+
+    expect(result?.code?.trim()).toEqual(expectedCode);
+  });
+
+  it('reads files using the provided encoding', () => {
+    const code = 'include("./fixtures/basic_javascript.js");';
+    const expectedCode = dummyFileContent;
+
+    const result = transform(code, {
+      filename: __filename,
+      plugins: [[babelPluginInclude, {encoding: 'latin1'}]],
+    });
+
+    expect(result?.code?.trim()).toEqual(expectedCode);
+  });
+
   afterAll(() => {
-    // Restore the original fs.readFileSync function after running the tests
+    // restore the original fs.readFileSync function after running the tests
     jest.restoreAllMocks();
   });
 });
